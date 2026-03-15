f2a1a7c6132648642ceaa210f69490b49e571dd8
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-15 02:09:58 +0100

    enrutamiento inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Main } from './components/main/main';
+import { C404 } from './components/c404/c404';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',pathMatch:'full',redirectTo:'home'},
+    {path:'home',component:Main},
+    {path:'**',component:C404}
+];
