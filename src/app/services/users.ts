0783a9a74ff948744ffed1f72d4cc33d7e3e96ac
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-15 02:16:03 +0100

    iniciar el servicio para inyectar en el home

--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/entironment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUserResponse } from '../interfaces/i-user-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Users {
-  
+  //private baseUrl='https://peticiones.online/api/users';
+  private baseUrl=environment.apiUrl;
+  httpClient=inject(HttpClient)
+
+  getAllUsers():Observable<IUserResponse>{
+    return this.httpClient.get<IUserResponse>(this.baseUrl);
+  }
 }
