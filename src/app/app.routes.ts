088f068f3d860d28fa4065b95fc320f767898af0
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-15 11:59:23 +0100

    implementar detalles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Main } from './components/main/main';
 import { C404 } from './components/c404/c404';
+import { Detalle } from './components/detalle/detalle';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Main},
+    {path:'user/:id',component:Detalle},
     {path:'**',component:C404}
 ];
