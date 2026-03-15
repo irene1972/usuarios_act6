9614edd7c4a023d49cf1e174745ba17ebff05348
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-15 02:38:15 +0100

    implementar home primer paso

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Users } from '../../services/users';
+import { IUser } from '../../interfaces/i-user';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './main.css',
 })
 export class Main {
+  usersService = inject(Users);
+  misUsuarios: IUser[] | undefined = undefined;
+  idUsuario: string = '';
+
+  constructor(private cd: ChangeDetectorRef, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+
+
+    this.usersService.getAllUsers().subscribe((data) => {
+      this.misUsuarios = data.results;
+      console.log(this.misUsuarios);
+      this.cd.detectChanges();
+    });
+  }
 
 }
