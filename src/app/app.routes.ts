7ee5249785619586b4e4c6235bd9970a78bfccc8
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-15 12:17:36 +0100

    implementar rutas para formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Main } from './components/main/main';
 import { C404 } from './components/c404/c404';
 import { Detalle } from './components/detalle/detalle';
+import { Formulario } from './components/formulario/formulario';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Main},
     {path:'user/:id',component:Detalle},
+    {path:'formulario',component:Formulario},
+    {path:'updateuser/:id',component:Formulario},
+    {path:'newuser',component:Formulario},
     {path:'**',component:C404}
 ];
