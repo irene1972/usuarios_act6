2341561f72bf2e1a58f7e7c657eb0498265cfb1a
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-03-20 12:07:21 +0100

    add regex a imagen

--- src/app/components/formulario/formulario.ts
+++ src/app/components/formulario/formulario.ts
@@ -32,11 +32,11 @@ export class Formulario {
       ]),
       email: new FormControl('', [
         Validators.required,
-        //Validators.email,
         Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
       ]),
       imagen: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
       ])
     }, []);
   }
